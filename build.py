575bb754c170f19347eb84104887575a38bb9686
Michael Seifert <mseifert@error-reports.org>
2016-10-16 17:10:35 +0200

    Added complex numbers example to template.

--- build.py
+++ build.py
@@ -9,6 +9,10 @@ with open('examples/fibonacci/test_fib.py') as test_fib_file:
     test_fib = test_fib_file.read()
     fib_test_blocks = test_fib.split('\n\n\n')
 
-slides = template.render(fib=fib_test_blocks)
+with open('examples/complex/test_complex.py') as test_complex_file:
+    test_complex = test_complex_file.read()
+    complex_test_blocks = test_complex.split('\n\n\n')
+
+slides = template.render(fib=fib_test_blocks, complex=complex_test_blocks)
 with open('slides.html', 'w') as output_file:
     output_file.write(slides)
