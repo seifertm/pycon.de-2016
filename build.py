fbc33789bbe1c09665a64d3863613caf39bb3354
Michael Seifert <mseifert@error-reports.org>
2016-10-16 17:17:35 +0200

    Build: Renamed split_test_case to split_test_suite.

--- build.py
+++ build.py
@@ -5,13 +5,13 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('slides.html')
 
-def split_test_case(test_case):
-    return test_case.split('\n\n\n')
+def split_test_suite(test_suite):
+    return test_suite.split('\n\n\n')
 
 def parse_test_suite(file_path):
     with open(file_path) as test_file:
         test_content = test_file.read()
-    test_blocks = split_test_case(test_content)
+    test_blocks = split_test_suite(test_content)
     return test_blocks
 
 fib_test_blocks = parse_test_suite('examples/fibonacci/test_fib.py')
