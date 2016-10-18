db964ed00f8f44d9c804256cb0b4d341be16620b
Michael Seifert <mseifert@error-reports.org>
2016-10-18 20:29:48 +0200

    Build: Added class Example which represents a set of code blocks.

--- build.py
+++ build.py
@@ -3,6 +3,13 @@
 import re
 from jinja2 import Environment, FileSystemLoader
 
+class Example:
+    def __init__(self, code_blocks):
+        self.code_blocks = code_blocks
+    
+    def __iter__(self):
+        return iter(self.code_blocks)
+
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('slides.html')
 
@@ -20,12 +27,12 @@ def parse_test_suite(file_path):
     test_content_without_comments = remove_comments(test_content)
     test_blocks = split_test_suite(test_content_without_comments)
     stripped_test_blocks = [test_block.strip() for test_block in test_blocks if test_block]
-    return stripped_test_blocks
+    return Example(stripped_test_blocks)
 
-fib_test_blocks = parse_test_suite('examples/fibonacci/test_fib.py')
-complex_test_blocks = parse_test_suite('examples/complex/test_complex.py')
-fraction_test_blocks = parse_test_suite('examples/fraction/test_fraction.py')
+fib_example = parse_test_suite('examples/fibonacci/test_fib.py')
+complex_example = parse_test_suite('examples/complex/test_complex.py')
+fraction_example = parse_test_suite('examples/fraction/test_fraction.py')
 
-slides = template.render(fib=fib_test_blocks, complex=complex_test_blocks, fraction=fraction_test_blocks)
+slides = template.render(fib=fib_example, complex=complex_example, fraction=fraction_example)
 with open('slides.html', 'w') as output_file:
     output_file.write(slides)
