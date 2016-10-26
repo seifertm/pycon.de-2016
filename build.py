ef10218e8f3b41922083bec20a2e82e6daee0393
Michael Seifert <mseifert@error-reports.org>
2016-10-26 19:36:17 +0200

    Added description of tasks to the examples.

--- build.py
+++ build.py
@@ -18,8 +18,9 @@ class CodeBlock:
         return self.segment
 
 class Example:
-    def __init__(self, title, blocks):
+    def __init__(self, title, task, blocks):
         self.title = title
+        self.task = task
         self.blocks = blocks
     
     def is_code_block(self, block):
@@ -53,10 +54,13 @@ def parse_test_suite(file_path):
     return blocks
 
 fib_example = Example(title='Write the implementation I',
+                      task='Implement the Fibonacci sequence. Copy the test cases into your test suite, one by one, and watch the new test fail. Type the simplest implementation that makes the currenty test work without breaking previous tests.',
                       blocks=parse_test_suite('examples/fibonacci/test_fib.py'))
 complex_example = Example(title='Write the implementation II',
+                          task='Implement a representation for complex numbers. Just as in the previous example, copy each test into your test suite, one after another and make the test work in the easiest way possible. Do not forget to refactor before adding a new test to your test suite.',
                           blocks=parse_test_suite('examples/complex/test_complex.py'))
 fraction_example = Example(title='Write the tests',
+                           task='Implement a representation for fractions. This time, the bodies of the tests are not provided. You have to implement them yourself according to the specification provided by the test name.',
                            blocks=parse_test_suite('examples/fraction/test_fraction.py'))
 
 slides = template.render(examples=[fib_example, complex_example, fraction_example])
